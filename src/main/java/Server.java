869da45bfa93dfe5ae3ad8a4d75291bfa9333714
eyupams <eyupvroegop@gmail.com>
2026-06-23 13:18:42 +0200

    fix

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -10,7 +10,7 @@ public class Server {
     private final ExecutorService executorServiceAcceptor = Executors.newSingleThreadExecutor();
     private final ExecutorService executorServiceMatchmaker = Executors.newSingleThreadExecutor();
     private final ExecutorService executorServiceGameHandling = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 10);
-    private final Storage memory = new JSONStorage();
+    public final Storage memory = new JSONStorage();
 
     public void start(int port) {
         executorServiceAcceptor.submit(() -> {
